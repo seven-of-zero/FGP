d1ef06907d977019791e9274d2a3733e41d505a1
Melakinf <52651672+Melakinf@users.noreply.github.com>
2020-02-26 09:59:58 +0800

    update 2

--- src/main/java/com/soz/utils/ExcelUtils.java
+++ src/main/java/com/soz/utils/ExcelUtils.java
@@ -20,18 +20,18 @@ public class ExcelUtils {
      */
     public static HSSFWorkbook getHSSFWorkbook(String sheetName, String[] title, String[][] values, HSSFWorkbook wb) {
 
-        // 第一步，创建一个HSSFWorkbook，对应一个Excel文件
+        // 创建一个HSSFWorkbook，对应一个Excel文件
         if (wb == null) {
             wb = new HSSFWorkbook();
         }
 
-        // 第二步，在workbook中添加一个sheet,对应Excel文件中的sheet
+        // 在workbook中添加一个sheet,对应Excel文件中的sheet
         HSSFSheet sheet = wb.createSheet(sheetName);
 
-        // 第三步，在sheet中添加表头第0行,注意老版本poi对Excel的行数列数有限制
+        // 在sheet中添加表头第0行,注意老版本poi对Excel的行数列数有限制
         HSSFRow row = sheet.createRow(0);
 
-        // 第四步，创建单元格，并设置值表头 设置表头居中
+        // 创建单元格，并设置值表头 设置表头居中
         HSSFCellStyle style = wb.createCellStyle();
         style.setAlignment(HorizontalAlignment.CENTER); // 创建一个居中格式
         //设置单元格宽度自适应，在此基础上把宽度调至1.5倍
